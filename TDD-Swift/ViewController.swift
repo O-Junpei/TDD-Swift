9eb5383546aaffbfcc80df57b31be8b8ae5f247f
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2019-06-26 12:54:18 +0900

    Initial Commit

---
+++ TDD-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDD-Swift
+//
+//  Created by 小野 純平 on 2019/06/26.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TDD-SwiftTests/TDD_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDD_SwiftTests.swift
+//  TDD-SwiftTests
+//
+//  Created by 小野 純平 on 2019/06/26.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import XCTest
+@testable import TDD_Swift
+
+class TDD_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
